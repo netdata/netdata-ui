8e4bd12bfd8f44e48de57444ddd68f3542e63a2e
DmitryM <rf00rm@gmail.com>
2019-10-10 21:22:43 +0700

    ui/bundle: script for production bundling created

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { default as Mock } from "./src/mock"
+export { default as createTheme } from "./src/theme/generic"
+export { MockTheme } from "./src/theme/mock/mock-theme"
+export { MockTheme2 } from "./src/theme/mock/mock-theme2"
