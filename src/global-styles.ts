7dff68bc64a56f603bba87dd54fd22d27c692766
Azein <azeinpwr@gmail.com>
2019-10-22 17:43:51 +0300

    fixed dynamic font reload

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -2,7 +2,6 @@ import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyles = createGlobalStyle`
   body {
-    @import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap');
     font-family: 'IBM Plex Sans', sans-serif;
   }
 `
