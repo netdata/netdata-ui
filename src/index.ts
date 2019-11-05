13a8fc39be77e8f31a36155520501be33865910b
VLegakis <vlegakis@gmail.com>
2019-11-05 17:18:25 +0200

    Root index restoration

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+export { Button, ButtonProps, ButtonType } from "./components/button"
+export { Sidebar, PortalSidebar } from "./components/sidebar"
+export { Icon, IconProps, iconsList } from "./components/icon"
+export { TextInput, useTouchedState, useFocusedState, useInputValue } from "./components/input"
+
+export {
+  H0,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  TextHuge,
+  TextBigger,
+  TextBig,
+  Text,
+  TextSmall,
+  TextNano,
+  TextFemto,
+  List,
+  ListItem,
+} from "./components/typography"
+
+export {
+  extendTheme,
+  getSizeUnit,
+  getSizeBy,
+  calcSize,
+  getOrElse,
+  propOrElse,
+  getColor,
+  getGutterHeight,
+} from "./theme"
+export { DefaultTheme } from "./theme/default"
+export { devices } from "./media"
+export { Checkbox, CheckboxProps, useCheckboxesList } from "./components/checkbox"
+export * from "./components/input"
