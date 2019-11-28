a96953dc7405fe0ba163ffc3bd304aa0284eee7e
builat <rf00rm@gmail.com>
2019-11-29 01:58:31 +0700

    table css override added (#43)

--- src/components/table/table.stories.tsx
+++ src/components/table/table.stories.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
+import styled from "styled-components"
 import { Table } from "./table"
 import { UserHeader } from "./components/UserHeader"
 import { readmeCleanup } from "../../../utils/readme"
 // @ts-ignore
 import readme from "./README.md"
+import { getColor } from "../../theme"
 
 const subData = {
   readme: {
@@ -18,6 +20,41 @@ sidebarStory.add(
   "Users table",
   () => (
     <Table
+      sortedBy={["user"]}
+      columns={UserHeader}
+      data={[
+        {
+          user: { photo: "https://i.pravatar.cc/30", name: "Fry", mail: "noway@noway.com" },
+          dots: "123",
+        },
+        {
+          user: { photo: "https://i.pravatar.cc/31", name: "Amy", mail: "amy@vong.com" },
+          dots: "123",
+        },
+        {
+          user: {
+            photo: "https://i.pravatar.cc/32",
+            name: "dr. Zoidberg",
+            mail: "drZ@planetmail.com",
+          },
+          dots: "123",
+        },
+      ]}
+      selectedItemsClb={items => console.log(items)}
+    />
+  ),
+  subData
+)
+
+const StyledTable = styled(Table)`
+  width: 800px;
+  background-color: ${getColor(["red", "roseWhite"])};
+`
+
+sidebarStory.add(
+  "Users with overrided style",
+  () => (
+    <StyledTable
       sortedBy={["user"]}
       columns={UserHeader}
       data={[
