53493b2ef9f0b6b33c8b9205cd770bdfab06c9fb
Azein <azeinpwr@gmail.com>
2020-02-12 00:01:00 +0300

    explicitly set font-family for typography elements (#66)

--- src/components/typography/H2.tsx
+++ src/components/typography/H2.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import { title } from "../../mixins"
 
 export const H2 = styled.h2`
+  font-family: "IBM Plex Sans", sans-serif;
   ${title}
   font-size: 22px;
   line-height: 24px;
