6c9a893813f3bc1bf4bd761558a3770619343797
VLegakis <vlegakis@gmail.com>
2020-02-25 16:54:57 +0200

    Remove disabled overlay for sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactNode } from "react"
-import { SidebarBox, ComponentBox, InfoBox, DisabledOverlay } from "./styled"
+import { SidebarBox, ComponentBox, InfoBox } from "./styled"
 
 export interface SidebarProps {
   info?: ReactNode
@@ -15,7 +15,6 @@ export const Sidebar: FC<SidebarProps> = ({
   right = false,
 }: SidebarProps) => (
   <ComponentBox isRight={right}>
-    <DisabledOverlay className={className} />
     <SidebarBox className={className} shadowSide={right}>
       {children}
     </SidebarBox>
