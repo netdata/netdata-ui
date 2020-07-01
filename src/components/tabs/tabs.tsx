a362581eaa7293664e03a18695d59c398da9cb55
Johnny Klironomos <ioannis.klironomos@gmail.com>
2020-07-01 22:29:16 +0300

    Introduced tabs components.

---
+++ src/components/tabs/tabs.tsx
@@ -0,0 +1,38 @@
+import React, { FC, ReactNode, useEffect } from "react"
+import { StyledTabsWrapper, StyledTabs } from "./styled"
+import { useSetActive, useBuildTabs, OnChange } from "./tabs-hooks"
+
+export interface TabsProps {
+  className?: string
+  onChange?: OnChange
+  selected?: number
+  children?: ReactNode
+  TabContent?: string | React.ComponentType<any>
+}
+
+export const Tabs: FC<TabsProps> = ({
+  className,
+  onChange,
+  selected,
+  children,
+  TabContent = "div",
+}: TabsProps) => {
+  const [activeIndex, setActiveIndex] = useSetActive(selected, onChange)
+
+  const [nav, content, firstActiveIndex, activeIsDisabled] = useBuildTabs(
+    children,
+    activeIndex,
+    setActiveIndex
+  )
+
+  useEffect(() => {
+    if (activeIsDisabled && activeIndex !== firstActiveIndex) setActiveIndex(firstActiveIndex)
+  }, [activeIndex, firstActiveIndex, activeIsDisabled, setActiveIndex])
+
+  return (
+    <StyledTabsWrapper className={className}>
+      <StyledTabs className="tabs">{nav}</StyledTabs>
+      <TabContent>{content}</TabContent>
+    </StyledTabsWrapper>
+  )
+}
