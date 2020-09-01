29197b3295ac572efa0d66f9003f120d8027e528
Azein <azeinpwr@gmail.com>
2020-09-01 19:41:13 +0300

    dynamic data update added

---
+++ src/components/filter-box/extended-filter-box.tsx
@@ -0,0 +1,12 @@
+import ReactFilterBox from "react-filter-box"
+
+// Library doesn't handle dynamic data, even given an updated handler instance in props
+// So we extend base component and update its parser manually
+// to enable autosuggestions for dynamic data
+export class ExtendedFilterBox extends ReactFilterBox {
+  componentDidUpdate(prevProps: any) {
+    if (prevProps.autoCompleteHandler !== this.props.autoCompleteHandler) {
+      this.parser.setAutoCompleteHandler(this.props.autoCompleteHandler)
+    }
+  }
+}
--- src/components/filter-box/filter-box.tsx
+++ src/components/filter-box/filter-box.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from "react"
 import { useDebounce } from "react-use"
-import ReactFilterBox from "react-filter-box"
+import { ExtendedFilterBox } from "./extended-filter-box"
 import "react-filter-box/lib/react-filter-box.css"
 import { Option } from "./types"
 import { Container, FilterContainer, MetaContainer, ResultsCount, FilterInfo } from "./styled"
@@ -72,10 +72,9 @@ export const FilterBox = ({
   const [filterQuery, setFilterQuery] = useState("")
   const [parsedQuery, setParsedQuery] = useState("")
 
-  const autoCompleteInstance = useMemo(
-    () => new AutoCompleteHandler(data, options, accessorPaths),
-    [AutoCompleteHandler, accessorPaths, data, options]
-  )
+  const autoCompleteInstance = useMemo(() => {
+    return new AutoCompleteHandler(data, options, accessorPaths)
+  }, [AutoCompleteHandler, accessorPaths, data, options])
   const handleError = (error, validationResult) => {
     if (onParseError) {
       onParseError(error, validationResult)
@@ -135,10 +134,11 @@ export const FilterBox = ({
     300,
     [displayedError]
   )
+
   return (
     <Container className={className}>
       <FilterContainer onBlur={handleBlur} onFocus={handleFocus} error={debouncedError}>
-        <ReactFilterBox
+        <ExtendedFilterBox
           {...props}
           autoCompleteHandler={autoCompleteInstance}
           onParseError={handleError}
