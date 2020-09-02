80c27190e4c2d358a230e9a4924f145b3d44307a
Azein <azeinpwr@gmail.com>
2020-09-02 15:11:31 +0300

    handle parent class update callback

--- src/components/filter-box/extended-filter-box.tsx
+++ src/components/filter-box/extended-filter-box.tsx
@@ -4,7 +4,10 @@ import ReactFilterBox from "react-filter-box"
 // So we extend base component and update its parser manually
 // to enable autosuggestions for dynamic data
 export class ExtendedFilterBox extends ReactFilterBox {
-  componentDidUpdate(prevProps: any) {
+  componentDidUpdate(prevProps: any, prevState: any) {
+    if (super.componentDidUpdate) {
+      super.componentDidUpdate(prevProps, prevState)
+    }
     if (prevProps.autoCompleteHandler !== this.props.autoCompleteHandler) {
       this.parser.setAutoCompleteHandler(this.props.autoCompleteHandler)
     }
