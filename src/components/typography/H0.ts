ac91ce407557aa1443ac7b01bba42f0fb8026188
Vassilis Legakis <vlegakis@gmail.com>
2020-10-15 12:55:10 +0300

    Import fix

--- src/components/typography/H0.ts
+++ src/components/typography/H0.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import { title } from "../../mixins/title"
+import { title } from "../../mixins"
 
 export const H0 = styled.h1`
   font-family: "IBM Plex Sans", sans-serif;
