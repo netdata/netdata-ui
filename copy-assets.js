6776ad93e146344067727989c4d09cabe889510f
John Apostolidis <john.apostolidi@gmail.com>
2020-11-20 16:24:59 +0200

    Export the flex component.

--- copy-assets.js
+++ copy-assets.js
@@ -5,3 +5,9 @@ cpx.copy("src/components/icon/assets/*.svg", "lib/components/icon/assets/", {})
 
 copyFileSync("src/mixins/types.d.ts", "lib/mixins/types.d.ts")
 copyFileSync("src/components/radio-button/index.d.ts", "lib/components/radio-button/index.d.ts")
+
+copyFileSync(
+  "src/components/templates/mixins/index.d.ts",
+  "lib/components/templates/mixins/index.d.ts"
+)
+copyFileSync("src/components/templates/flex/index.d.ts", "lib/components/templates/flex/index.d.ts")
