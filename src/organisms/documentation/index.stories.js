da59e42aa5c5df7ded5f12c8ad9b8a32797e12dd
novykh <ioannis.klironomos@gmail.com>
2020-12-17 16:38:53 +0200

    Introduced documentation help button organism.

---
+++ src/organisms/documentation/index.stories.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+import { storiesOf } from "@storybook/react"
+import { readmeCleanup } from "utils/readme"
+import Flex from "src/components/templates/flex"
+import { Button } from "src/components/button"
+// @ts-ignore
+import readme from "./README.md"
+import Documentation from "./index"
+
+const subData = {
+  readme: {
+    sidebar: readmeCleanup(readme),
+  },
+  jest: ["index.test.js"],
+}
+
+const story = storiesOf("ORGANISMS|Documentation/Documentation", module)
+
+story.add(
+  "Cloud Documentation",
+  () => {
+    return (
+      <Flex background="mainBackgroundDisabled" width="100vw" height="100vh">
+        <Documentation app="cloud">
+          {(toggle, isOpen) => (
+            <Button
+              width="auto"
+              onClick={toggle}
+              label={`${isOpen ? "hide" : "show"} documentation modal`}
+            />
+          )}
+        </Documentation>
+      </Flex>
+    )
+  },
+  subData
+)
