ad9004dc1c05ff5f2d5c5dada52478718a6d87b0
apostolidhs <john.apostolidi@gmail.com>
2021-02-16 18:10:21 +0200

    Added typography white space.

--- src/components/typography/mixins/index.d.ts
+++ src/components/typography/mixins/index.d.ts
@@ -2,6 +2,10 @@ export interface TextAlignType {
   textAlign?: "left" | "center" | "right"
 }
 
+export interface WhiteSpaceProps {
+  whiteSpace?: "normal" | "nowrap"
+}
+
 export interface WordBreakProps {
   wordBreak?: "normal" | "break-all" | "keep-all" | "break-word"
 }
