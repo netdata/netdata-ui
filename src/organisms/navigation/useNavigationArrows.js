0ddb7e0ac57986b48c777c67bdb1b60da0b06e9c
allelos <p.paliagkas@gmail.com>
2021-02-17 13:11:06 +0200

    Created tabbed navigation hooks.

---
+++ src/organisms/navigation/useNavigationArrows.js
@@ -0,0 +1,35 @@
+import { useCallback, useState, useLayoutEffect } from "react"
+
+export default (ref, childrenRef, ...deps) => {
+  const [arrowLeft, setArrowLeft] = useState(false)
+  const [arrowRight, setArrowRight] = useState(false)
+
+  const onScroll = useCallback(() => {
+    if (!ref.current || !childrenRef.current) return
+    const container = ref.current
+    const tabs = childrenRef.current
+    const [firstTab] = tabs
+    const lastTab = tabs[tabs.length - 1]
+
+    const scroll = container.scrollLeft
+    const { width } = container.getBoundingClientRect()
+    const { left } = lastTab.getBoundingClientRect()
+    const { width: firstTabWidth } = firstTab.getBoundingClientRect()
+
+    if (left > scroll) setArrowRight(true)
+    if (left < scroll || left < width) setArrowRight(false)
+
+    if (scroll > firstTabWidth) setArrowLeft(true)
+    if (scroll < firstTabWidth) setArrowLeft(false)
+  }, [])
+
+  useLayoutEffect(() => {
+    if (!ref.current || !childrenRef.current.length) return
+    const container = ref.current
+    onScroll()
+    container.addEventListener("scroll", onScroll)
+    return () => container.removeEventListener("scroll", onScroll)
+  }, [...deps])
+
+  return [arrowLeft, arrowRight]
+}
---
+++ src/organisms/navigation/useNavigationScroll.js
@@ -0,0 +1,16 @@
+import { useCallback, useEffect } from "react"
+
+export default ref => {
+  const onWheel = useCallback(e => {
+    if (!ref.current) return
+    const container = ref.current
+    container.scrollLeft = container.scrollLeft + e.deltaY * 0.1
+  })
+
+  useEffect(() => {
+    if (!ref.current) return
+    const container = ref.current
+    container.addEventListener("wheel", onWheel)
+    return () => container.remove("wheel", onWheel)
+  }, [])
+}
