559e4d63e4b21fc002588b0ff193d9fece397981
novykh <ioannis.klironomos@gmail.com>
2021-02-19 18:42:32 +0200

    Updated collapsible to work in both directions.

--- src/components/collapsible/index.js
+++ src/components/collapsible/index.js
@@ -4,63 +4,72 @@ import styled from "styled-components"
 import useForwardRef from "src/hooks/use-forward-ref"
 import Flex from "src/components/templates/flex"
 
+const measurementByDimension = {
+  vertical: "height",
+  horizontal: "width",
+}
+
 const Animated = styled(Flex).attrs({
   column: true,
 })`
-  transition: ${({ duration }) => `max-height ${duration}ms ease-out, opacity ${duration}ms ease`};
-  max-height: ${({ maxHeight }) => maxHeight};
+  transition: ${({ duration, measurement }) =>
+    `max-${measurement} ${duration}ms ease-out, opacity ${duration}ms ease`};
+  ${({ measurement, maxDimension }) => `max-${measurement}: ${maxDimension}`};
 `
 
-const Collapsible = forwardRef(({ open = false, duration = 150, children, ...rest }, parentRef) => {
-  duration = process.env.NODE_ENV === "test" ? 0 : duration
+const Collapsible = forwardRef(
+  ({ open = false, duration = 150, children, direction, ...rest }, parentRef) => {
+    duration = process.env.NODE_ENV === "test" ? 0 : duration
 
-  const [height, setHeight] = useState(open ? "initial" : 0)
-  const [animatedOpen, setAnimatedOpen] = useState(open)
-  const [ref, setRef] = useForwardRef(parentRef)
+    const [dimension, setDimension] = useState(open ? "initial" : 0)
+    const [animatedOpen, setAnimatedOpen] = useState(open)
+    const [ref, setRef] = useForwardRef(parentRef)
 
-  useUpdateEffect(() => {
-    let nestedRequestId
-    const requestId = requestAnimationFrame(() => {
-      setHeight(!open ? `${ref.current.scrollHeight}px` : 0)
-      nestedRequestId = requestAnimationFrame(() => {
-        setHeight(open ? `${ref.current.scrollHeight}px` : 0)
+    useUpdateEffect(() => {
+      let nestedRequestId
+      const requestId = requestAnimationFrame(() => {
+        setDimension(!open ? `${ref.current.scrollHeight}px` : 0)
+        nestedRequestId = requestAnimationFrame(() => {
+          setDimension(open ? `${ref.current.scrollHeight}px` : 0)
+        })
       })
-    })
 
-    if (open) {
-      setAnimatedOpen(true)
-    }
+      if (open) {
+        setAnimatedOpen(true)
+      }
 
-    const timeoutId = setTimeout(
-      () => (open ? setHeight("initial") : setAnimatedOpen(false)),
-      duration
+      const timeoutId = setTimeout(
+        () => (open ? setDimension("initial") : setAnimatedOpen(false)),
+        duration
+      )
+
+      return () => {
+        cancelAnimationFrame(requestId)
+        cancelAnimationFrame(nestedRequestId)
+        clearTimeout(timeoutId)
+      }
+    }, [open])
+
+    const child = useMemo(
+      () => animatedOpen && (typeof children === "function" ? children() : children),
+      [animatedOpen, children]
     )
 
-    return () => {
-      cancelAnimationFrame(requestId)
-      cancelAnimationFrame(nestedRequestId)
-      clearTimeout(timeoutId)
-    }
-  }, [open])
-
-  const child = useMemo(
-    () => animatedOpen && (typeof children === "function" ? children() : children),
-    [animatedOpen, children]
-  )
-
-  return (
-    <Animated
-      open={open}
-      maxHeight={height}
-      duration={duration}
-      ref={setRef}
-      data-testid="collapsible"
-      overflow={height === "initial" ? "visible" : "hidden"}
-      {...rest}
-    >
-      {child}
-    </Animated>
-  )
-})
+    return (
+      <Animated
+        open={open}
+        maxDimension={dimension}
+        measurement={measurementByDimension[direction] || measurementByDimension.vertical}
+        duration={duration}
+        ref={setRef}
+        data-testid="collapsible"
+        overflow={dimension === "initial" ? "visible" : "hidden"}
+        {...rest}
+      >
+        {child}
+      </Animated>
+    )
+  }
+)
 
 export default memo(Collapsible)
