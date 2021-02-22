2bf66afc82c207535125808ffbd32b19c5249794
allelos <p.paliagkas@gmail.com>
2021-02-22 11:36:54 +0200

    Fixed resize and pagination handling.

--- src/organisms/navigation/draggable/index.js
+++ src/organisms/navigation/draggable/index.js
@@ -1,42 +1,43 @@
-import React, { useMemo, useCallback, useRef, useEffect } from "react"
+import React, { useMemo, useCallback, useRef, useLayoutEffect } from "react"
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 import Flex from "src/components/templates/flex"
-import { Icon } from "src/components/icon/icon"
 import useNavigationArrows from "src/organisms/navigation/hooks/useNavigationArrows"
 import useNavigationScroll from "src/organisms/navigation/hooks/useNavigationScroll"
+import useOnTabsResize from "src/organisms/navigation/hooks/useOnResize"
 import Container from "./container"
+import Arrow from "./arrow"
 
-const DraggableTabs = ({ children, onDragEnd: dragEnd, onTabClose, onResize: resizeCallback }) => {
-  const containerRef = useRef()
+const DraggableTabs = ({ children, onDragEnd: dragEnd, onTabClose, onResize, collapsed }) => {
+  const ref = useRef()
   const tabsRef = useRef([])
 
-  const [arrowLeft, arrowRight] = useNavigationArrows(containerRef, tabsRef, [
-    children,
-  ])
-  useNavigationScroll(containerRef)
+  useNavigationScroll(ref)
+  const onTabsResize = useOnTabsResize(ref, tabsRef, onResize, [children])
+  const [arrowLeft, arrowRight, onScroll] = useNavigationArrows(ref, tabsRef, children, collapsed)
 
-  const onResize = useCallback(() => {
-    if (!containerRef.current || !tabsRef.current.length) return
+  useLayoutEffect(() => {
+    onTabsResize()
+    onScroll()
 
-    const container = containerRef.current
-    const { right: containerRight } = container.getBoundingClientRect()
+    const container = ref.current
 
-    const lastTab = tabsRef.current[tabsRef.current.length - 1]
-    const { right: tabRight, width: tabWidth } = lastTab.getBoundingClientRect()
-
-    if (tabRight > containerRight) resizeCallback(true)
-    if (tabRight + tabWidth <= containerRight) resizeCallback(false)
-  }, [children])
-
-  useEffect(() => {
-    onResize()
-    window.addEventListener("resize", onResize)
-    return () => window.removeEventListener("resize", onResize)
-  }, [children])
+    container.addEventListener("scroll", onScroll)
+    window.addEventListener("resize", () => {
+      onTabsResize()
+      onScroll()
+    })
+    return () => {
+      container.removeEventListener("scroll", onScroll)
+      window.removeEventListener("resize", () => {
+        onTabsResize()
+        onScroll()
+      })
+    }
+  }, [collapsed, children])
 
   const scrollLeft = e => {
     e.preventDefault()
-    const container = containerRef.current
+    const container = ref.current
     container.scrollTo({
       left: container.scrollLeft - 100,
       behavior: "smooth",
@@ -45,7 +46,7 @@ const DraggableTabs = ({ children, onDragEnd: dragEnd, onTabClose, onResize: res
 
   const scrollRight = e => {
     e.preventDefault()
-    const container = containerRef.current
+    const container = ref.current
     container.scrollTo({
       left: container.scrollLeft + 100,
       behavior: "smooth",
@@ -55,10 +56,14 @@ const DraggableTabs = ({ children, onDragEnd: dragEnd, onTabClose, onResize: res
   const setTabRef = useCallback(
     tab => {
       if (!tab) return
-      if (children.length >= tabsRef.current.length) tabsRef.current = [...tabsRef.current, tab]
-      if (children.length < tabsRef.current.length) {
-        tabsRef.current = tabsRef.current.filter(
-          t => t.getAttribute("data-rbd-draggable-id") === tab.getAttribute("data-rbd-draggable-id")
+
+      const tabs = tabsRef.current
+
+      if (children.length >= tabs.length) tabsRef.current = [...tabs, tab]
+      if (children.length < tabs.length) {
+        tabsRef.current = tabs.filter(
+          node =>
+            node.getAttribute("data-rbd-draggable-id") === tab.getAttribute("data-rbd-draggable-id")
         )
       }
     },
@@ -96,22 +101,13 @@ const DraggableTabs = ({ children, onDragEnd: dragEnd, onTabClose, onResize: res
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Flex flex="grow" basis="0%" height="100%" width="100%" alignItems="center" overflow="hidden">
-        {arrowLeft && (
-          <Icon
-            name="navLeft"
-            height={8}
-            width={8}
-            color="text"
-            onClick={scrollLeft}
-            margin={[0, 2]}
-          />
-        )}
+        {arrowLeft && <Arrow onClick={scrollLeft} name="navLeft" />}
         <Droppable droppableId="tabList" direction="horizontal">
           {({ innerRef, placeholder, droppableProps }) => {
             return (
               <Container
                 ref={node => {
-                  containerRef.current = node
+                  ref.current = node
                   innerRef(node)
                 }}
                 {...droppableProps}
@@ -126,16 +122,7 @@ const DraggableTabs = ({ children, onDragEnd: dragEnd, onTabClose, onResize: res
             )
           }}
         </Droppable>
-        {arrowRight && (
-          <Icon
-            name="navRight"
-            height={8}
-            width={8}
-            color="text"
-            onClick={scrollRight}
-            margin={[0, 2]}
-          />
-        )}
+        {arrowRight && <Arrow onClick={scrollRight} name="navRight" />}
       </Flex>
     </DragDropContext>
   )
--- src/organisms/navigation/hooks/useNavigationArrows.js
+++ src/organisms/navigation/hooks/useNavigationArrows.js
@@ -1,35 +1,27 @@
-import { useCallback, useState, useLayoutEffect } from "react"
+import { useCallback, useState } from "react"
 
-export default (ref, childrenRef, deps) => {
+export default (ref, childrenRef, children, collapsed) => {
   const [arrowLeft, setArrowLeft] = useState(false)
   const [arrowRight, setArrowRight] = useState(false)
 
   const onScroll = useCallback(() => {
     if (!ref.current || !childrenRef.current) return
+    if (!collapsed) return
+
     const container = ref.current
     const tabs = childrenRef.current
-    const [firstTab] = tabs
     const lastTab = tabs[tabs.length - 1]
 
     const scroll = container.scrollLeft
-    const { width } = container.getBoundingClientRect()
-    const { left } = lastTab.getBoundingClientRect()
-    const { width: firstTabWidth } = firstTab.getBoundingClientRect()
+    const { right: containerRight } = container.getBoundingClientRect()
+    const { right: lastTabRight } = lastTab.getBoundingClientRect()
 
-    if (left > scroll) setArrowRight(true)
-    if (left < scroll || left < width) setArrowRight(false)
+    if (lastTabRight > containerRight) setArrowRight(true)
+    if (lastTabRight <= containerRight) setArrowRight(false)
 
     if (scroll > 0) setArrowLeft(true)
     if (scroll === 0) setArrowLeft(false)
-  }, [])
+  }, [collapsed, children])
 
-  useLayoutEffect(() => {
-    if (!ref.current || !childrenRef.current.length) return
-    const container = ref.current
-    onScroll()
-    container.addEventListener("scroll", onScroll)
-    return () => container.removeEventListener("scroll", onScroll)
-  }, deps)
-
-  return [false, false, setArrowRight]
+  return [arrowLeft, arrowRight, onScroll]
 }
