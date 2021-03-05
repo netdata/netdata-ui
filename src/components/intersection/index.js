0b4749c7a33042f402918cf45d948db097c03ae1
apostolidhs <john.apostolidi@gmail.com>
2021-03-05 19:37:20 +0200

    Fixed intersection component.

--- src/components/intersection/index.js
+++ src/components/intersection/index.js
@@ -12,10 +12,10 @@ const Intersection = ({
   children,
   ...rest
 }) => {
-  const [ref, visible] = useIntersection({ root, rootMargin, threshold })
+  const [setRef, , visible] = useIntersection({ root, rootMargin, threshold })
 
   return (
-    <Flex ref={ref} height={{ min: height }} width={width} {...rest}>
+    <Flex ref={setRef} height={{ min: height }} width={width} {...rest}>
       {visible ? children() : fallback}
     </Flex>
   )
