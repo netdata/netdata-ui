9790dce7edc50f15e5967b1651c4ef09c2fdd346
allelos <p.paliagkas@gmail.com>
2021-04-14 14:38:08 +0300

    Added support for discourse topics.

--- src/organisms/documentation/search/provider.js
+++ src/organisms/documentation/search/provider.js
@@ -1,6 +1,8 @@
 import React from "react"
 import SiteSearchAPIConnector from "@elastic/search-ui-site-search-connector"
 import { SearchProvider as ElasticSearchProvider, WithSearch } from "@elastic/react-search-ui"
+import DiscourseSearch from "./discourse"
+import { getResultsByKey } from "./utils"
 
 const apiConnector = new SiteSearchAPIConnector({
   engineKey: "BZL_aEiLAebVKkcm3eFr",
@@ -37,14 +39,19 @@ const config = {
 const mapContextToProps = ({ searchTerm, setSearchTerm, results, reset }) => ({
   searchTerm,
   setSearchTerm,
-  results,
+  results: {
+    ...getResultsByKey(results),
+    discourse: [],
+  },
   reset,
 })
 
 const SearchProvider = ({ children }) => {
   return (
     <ElasticSearchProvider config={config}>
-      <WithSearch mapContextToProps={mapContextToProps}>{props => children(props)}</WithSearch>
+      <WithSearch mapContextToProps={mapContextToProps}>
+        {props => <DiscourseSearch {...props}>{children}</DiscourseSearch>}
+      </WithSearch>
     </ElasticSearchProvider>
   )
 }
