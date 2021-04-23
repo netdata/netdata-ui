e09451656342e660f017a63e3afd3ca1748c92ee
novykh <ioannis.klironomos@gmail.com>
2021-04-23 11:10:27 +0300

    Fixed infinite loop issue on tab widths calculation.

--- src/organisms/navigation/hooks/useOnTabsResize.js
+++ src/organisms/navigation/hooks/useOnTabsResize.js
@@ -1,6 +1,8 @@
 import { useCallback, useRef } from "react"
 
 export default (ref, target, callback, deps) => {
+  const prevValuesRef = useRef({})
+
   return useCallback(() => {
     if (!ref.current || !target.current.length) return
 
@@ -10,7 +12,15 @@ export default (ref, target, callback, deps) => {
     const lastTab = target.current[target.current.length - 1]
     const { right: tabRight, width: tabWidth } = lastTab.getBoundingClientRect()
 
+    const { prevContainerRight, prevTabWidth } = prevValuesRef.current
+    if (prevContainerRight === containerRight && prevTabWidth === tabWidth) return
+
+    prevValuesRef.current = {
+      prevContainerRight: containerRight,
+      prevTabWidth: tabWidth,
+    }
+
     if (tabRight >= containerRight) return callback(true)
     if (tabRight + tabWidth < containerRight) return callback(false)
-  }, [deps])
+  }, deps)
 }
