b8ef7c1e2d683f58307af53cf1be699b451667a0
allelos <p.paliagkas@gmail.com>
2021-05-26 10:26:50 +0300

    Created Icon Pill component.

---
+++ src/components/pill/icon.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Icon } from "src/components/icon"
+import getPillColor from "./colors"
+
+const PillIcon = ({ icon, color, hollow, flavour }) => {
+  if (!icon) return null
+  if (typeof icon !== "string") return icon
+
+  return (
+    <Icon
+      width="14px"
+      height="14px"
+      name={icon}
+      color={color ? color : hollow ? getPillColor("color", flavour) : "bright"}
+    />
+  )
+}
+
+export default PillIcon
