4bb0d24dc743b00353a8d0917c092d778be225c9
allelos <p.paliagkas@gmail.com>
2021-06-24 13:24:42 +0300

    Created news organism story.

---
+++ src/organisms/news/index.stories.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react"
+import { useLocalStorage } from "react-use"
+import { storiesOf } from "@storybook/react"
+import { readmeCleanup } from "utils/readme"
+import Pill from "src/components/pill"
+import readme from "./README.md"
+import News from "./index"
+
+const subData = {
+  readme: {
+    sidebar: readmeCleanup(readme),
+  },
+  jest: ["index.test.js"],
+}
+
+const story = storiesOf("ORGANISMS|News/News", module)
+
+const Wrapper = ({ children }) => {
+  const [, setLastSeen] = useLocalStorage("news_last_seen")
+
+  useEffect(() => {
+    return () => setLastSeen(null)
+  }, [])
+
+  return children
+}
+
+const CloudNews = () => (
+  <Wrapper>
+    <News app="cloud">
+      {({ toggle, upToDate }) => (
+        <Pill icon="insights" onClick={toggle} hollow flavour={upToDate ? "neutral" : "success"}>
+          Cloud news
+        </Pill>
+      )}
+    </News>
+  </Wrapper>
+)
+
+const AgentNews = () => (
+  <Wrapper>
+    <News app="agent">
+      {({ toggle, upToDate }) => (
+        <Pill icon="insights" onClick={toggle} hollow flavour={upToDate ? "neutral" : "success"}>
+          Agent news
+        </Pill>
+      )}
+    </News>
+  </Wrapper>
+)
+
+story.add("Cloud News", CloudNews, subData)
+story.add("Agent News", AgentNews, subData)
