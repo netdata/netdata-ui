a461406a2b0b7d4a554559b3785fdf3a701ec5d4
jjtsou <itsourtis@gmail.com>
2021-07-02 18:20:57 +0300

    show an icon for sorting on react-table

--- src/components/table/components/column-head/column-head.js
+++ src/components/table/components/column-head/column-head.js
@@ -1,4 +1,6 @@
 import React, { useContext } from "react"
+import { Icon } from "src/components/icon"
+import Flex from "src/components/templates/flex"
 import { LayoutContext } from "../../layout-context"
 
 export const ColumnHead = ({ column, sortableBy, customProps }) => {
@@ -6,7 +8,14 @@ export const ColumnHead = ({ column, sortableBy, customProps }) => {
   const sortProps = sortableBy.includes(column.id) ? column.getSortByToggleProps() : {}
   return layoutType === "table" ? (
     <th {...sortProps} {...column.getHeaderProps()}>
-      {column.render("Header", { ...customProps })}
+      <Flex alignItems="center" justifyContent="start" height={{ min: 6 }} gap={2}>
+        {column.render("Header", { ...customProps })}
+        {column.isSorted ? (
+          <Icon name="arrow_down" color="selected" rotate={column.isSortedDesc ? null : 2} />
+        ) : (
+          ""
+        )}
+      </Flex>
     </th>
   ) : (
     <div {...sortProps} {...column.getHeaderProps()} className="column-head">
