6675694d10a35f64625bf945a7d604593b843213
jjtsou <itsourtis@gmail.com>
2021-07-05 15:31:39 +0300

    show sortingIndicator on hover on column head

--- src/components/table/components/column-head/column-head.js
+++ src/components/table/components/column-head/column-head.js
@@ -1,25 +1,46 @@
-import React, { useContext } from "react"
+import React, { useContext, useState, useMemo } from "react"
 import { Icon } from "src/components/icon"
 import Flex from "src/components/templates/flex"
 import { LayoutContext } from "../../layout-context"
 
 export const ColumnHead = ({ column, sortableBy, customProps }) => {
+  const [hover, setHover] = useState(false)
   const layoutType = useContext(LayoutContext)
-  const sortProps = sortableBy.includes(column.id) ? column.getSortByToggleProps() : {}
+  const {
+    id,
+    getSortByToggleProps,
+    getHeaderProps,
+    render,
+    canSort,
+    isSorted,
+    isSortedDesc,
+  } = column
+  const showHoverIndicator = canSort && hover
+  const sortProps = useMemo(() => (sortableBy.includes(id) ? getSortByToggleProps() : {}), [id])
   return layoutType === "table" ? (
-    <th {...sortProps} {...column.getHeaderProps()}>
+    <th
+      {...sortProps}
+      {...getHeaderProps()}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       <Flex alignItems="center" justifyContent="start" height={{ min: 6 }} gap={2}>
-        {column.render("Header", { ...customProps })}
-        {column.isSorted ? (
-          <Icon name="arrow_down" color="selected" rotate={column.isSortedDesc ? null : 2} />
+        {render("Header", { ...customProps })}
+        {isSorted ? (
+          <Icon
+            name="arrow_down"
+            color="selected"
+            rotate={isSortedDesc ? null : 2}
+            data-testid="columnHhead-sortingIcon"
+          />
         ) : (
-          ""
+          showHoverIndicator && <Icon name="arrow_down" color="selected" rotate={2} />
         )}
       </Flex>
     </th>
   ) : (
-    <div {...sortProps} {...column.getHeaderProps()} className="column-head">
-      {column.render("Header", { ...customProps })}
+    <div {...sortProps} {...getHeaderProps()} className="column-head">
+      {render("Header", { ...customProps })}
     </div>
   )
 }
