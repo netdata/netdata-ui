8a93907c1348e88bd67a3e9e05f6971953e62e58
ALex_Nti <a.ntitoras@gmail.com>
2021-12-23 17:43:34 +0200

    apply height and padding to input container

--- src/components/input/use-input-styles.js
+++ src/components/input/use-input-styles.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useMemo, useCallback } from "react"
 
 const makeColor = ({
@@ -75,27 +74,6 @@ const useInputStyles = ({ size = "large", disabled, success, error, focused }) =
   )
 
   return { styles: { inputContainer, iconContainer } }
-=======
-const useInputStyles = ({ size }) => {
-  switch (size) {
-    case "tiny":
-      return {
-        height: "30px",
-        padding: "5px 8px 5px 8px",
-      }
-    case "small":
-      return {
-        height: "34px",
-        padding: "7px 12px 7px 12px",
-      }
-    default: {
-      return {
-        height: "42px",
-        padding: "11px 16px 11px 16px",
-      }
-    }
-  }
->>>>>>> add input styles
 }
 
 export default useInputStyles
