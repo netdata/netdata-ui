757940a9d4703d8f7dc8a515fc79db1682d092ea
ALex_Nti <a.ntitoras@gmail.com>
2021-12-24 10:42:29 +0200

    add styles to useStyle hook

--- src/components/input/use-input-styles.js
+++ src/components/input/use-input-styles.js
@@ -1,22 +1,64 @@
-const useInputStyles = ({ size }) => {
-  switch (size) {
-    case "tiny":
-      return {
-        height: "30px",
-        padding: "5px 8px 5px 8px",
-      }
-    case "small":
-      return {
-        height: "34px",
-        padding: "7px 12px 7px 12px",
-      }
-    default: {
-      return {
-        height: "42px",
-        padding: "11px 16px 11px 16px",
+import React from "react"
+
+const makeBorderColor = () => ({
+  success: "success",
+  error: "error",
+  disabled: "inputBorder",
+  default: "inputBorder",
+})
+
+const makeShadowColor = () => ({
+  success: "success",
+  error: "error",
+  disabled: "inputBorder",
+  default: "controlFocused",
+})
+
+const useInputStyles = ({ size, disabled, success, error, focused }) => {
+  const defaultStyles = {
+    width: "100%",
+  }
+  const status = success ? "success" : error ? "error" : disabled ? "disabled" : "default"
+
+  const sizeStyles = React.useCallback(() => {
+    switch (size) {
+      case "tiny":
+        return {
+          height: "30px",
+          padding: [1.2, 2, 1.2, 2],
+        }
+      case "small":
+        return {
+          height: "34px",
+          padding: [1.8, 3, 1.8, 3],
+        }
+      default: {
+        return {
+          height: "42px",
+          padding: [2.8, 4, 2.8, 4],
+        }
       }
     }
-  }
+  }, [size])
+
+  const styles = React.useMemo(() => {
+    return {
+      inputContainer: {
+        ...defaultStyles,
+        ...sizeStyles(),
+        border: {
+          size: "1px",
+          type: "solid",
+          color: makeBorderColor()[status],
+          side: "all",
+        },
+        boxShadow: focused ? { size: "0 0 0 1px", color: makeShadowColor()[status] } : null,
+        round: true,
+      },
+    }
+  }, [status, sizeStyles])
+
+  return { styles }
 }
 
 export default useInputStyles
