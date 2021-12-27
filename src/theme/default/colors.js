547ba42ce49ac97530ab3cd64c6ec957df4fd4a1
ALex_Nti <a.ntitoras@gmail.com>
2021-12-27 16:18:40 +0200

    change hover and active input border colors

--- src/theme/default/colors.js
+++ src/theme/default/colors.js
@@ -46,8 +46,8 @@ const appColors = {
   panel: rawColors.neutral.limedSpruce,
   //Input colors
   inputBorder: rawColors.neutral.iron,
-  inputHover: rawColors.neutral.regentgrey,
-  inputFocus: rawColors.neutral.bluebayoux,
+  inputBorderHover: rawColors.neutral.regentgrey,
+  inputBorderFocus: rawColors.neutral.bluebayoux,
 }
 
 export default {
