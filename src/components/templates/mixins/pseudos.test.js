b048e137d63d185429417635744f2c1c47a8a5d5
ALex_Nti <a.ntitoras@gmail.com>
2021-12-28 14:06:02 +0200

    add test for pseudos

---
+++ src/components/templates/mixins/pseudos.test.js
@@ -0,0 +1,44 @@
+import pseudos from "./pseudos"
+import { DefaultTheme as theme } from "src/theme/default"
+
+const disabledColor = theme.colors.disabled
+it("renders", () => {
+  expect(pseudos({ theme })).toBe("")
+})
+
+it("render box shadow (hover) ", () => {
+  const size = "0 0 0 1px"
+  const _hover = { boxShadow: { size, color: "disabled" } }
+  const pseudo = `
+&:hover {
+box-shadow:${size} ${disabledColor};
+}`
+
+  expect(pseudos({ theme, _hover })).toBe(pseudo)
+})
+
+it("render border (hover)", () => {
+  const _hover = { border: { color: "disabled", side: "top", size: "1rem", type: "dashed" } }
+  const pseudo = `
+&:hover {
+border-top: 1rem dashed ${disabledColor};
+}`
+
+  expect(pseudos({ theme, _hover })).toBe(pseudo)
+})
+
+it("render border and box shadow (hover)", () => {
+  const size = "0 0 0 1px"
+
+  const _hover = {
+    border: { color: "disabled", side: "top", size: "1rem", type: "dashed" },
+    boxShadow: { size, color: "disabled" },
+  }
+  const pseudo = `
+&:hover {
+box-shadow:${size} ${disabledColor};
+border-top: 1rem dashed ${disabledColor};
+}`
+
+  expect(pseudos({ theme, _hover })).toBe(pseudo)
+})
