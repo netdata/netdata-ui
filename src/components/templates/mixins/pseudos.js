0abfd109044a020a931c0da35a26e1f8acd62b3d
ALex_Nti <a.ntitoras@gmail.com>
2021-12-29 12:12:47 +0200

    add dynamic set of pseudoselectors

--- src/components/templates/mixins/pseudos.js
+++ src/components/templates/mixins/pseudos.js
@@ -1,20 +1,66 @@
-import shadow from "src/components/templates/mixins/shadow"
+import background from "src/components/templates/mixins/background"
 import borderMixIn from "src/components/templates/mixins/border"
+import shadow from "src/components/templates/mixins/shadow"
 
 const clearEmptyLines = str => str.replace(/^(?=\n)$|^\s*|\s*$|\n\n+/gm, "")
 
-export default ({ _hover, theme }) => {
-  let pseudo = ""
-  if (_hover) {
-    const { boxShadow, border } = _hover
-    const shadowProp = boxShadow ? `${shadow({ boxShadow, theme })}` : ""
-    const borderProps = border ? `${borderMixIn({ theme, border })}` : ""
+export const callAllFunctionsAndMergeResults = (...fns) => {
+  let result = ""
+  return function mergedFn(arg) {
+    fns.forEach(fn => {
+      const functionResult = fn && typeof fn === "function" ? fn(arg) : ""
+      result = result + functionResult
+    })
+    return result
+  }
+}
 
-    pseudo = `
-    &:hover {
-        ${shadowProp}
-        ${borderProps}
-    }`
+export const pseudoSelectors = {
+  _before: "&::before",
+  _after: "&::after",
+  _hover: "&:hover, &[data-hover]",
+  _active: "&:active, &[data-active]",
+  _focus: "&:focus, &[data-focus]",
+  _focusWithin: "&:focus-within",
+  _visited: "&:visited",
+  _empty: "&:empty",
+  _even: "&:nth-of-type(even)",
+  _odd: "&:nth-of-type(odd)",
+  _disabled: "&[disabled], &[aria-disabled=true], &[data-disabled]",
+  _checked: "&[aria-checked=true]",
+  _mixed: "&[aria-checked=mixed]",
+  _selected: "&[aria-selected=true], [data-selected] > &",
+  _invalid: "&[aria-invalid=true]",
+  _pressed: "&[aria-pressed=true]",
+  _readOnly: "&[aria-readonly=true], &[readonly]",
+  _first: "&:first-of-type",
+  _last: "&:last-of-type",
+  _expanded: "&[aria-expanded=true]",
+  _grabbed: "&[aria-grabbed=true]",
+  _notFirst: "&:not(:first-of-type)",
+  _notLast: "&:not(:last-of-type)",
+  _groupHover: "[role=group]:hover &",
+  _autofill: "&:-webkit-autofill",
+  _placeholder: "&::placeholder",
+}
+
+export default ({ theme, ...props }) => {
+  let pseudo = ""
+  for (const prop in props) {
+    if (prop in pseudoSelectors) {
+      const pseudoProp = prop
+      const pseudoStyles = props[pseudoProp]
+      const styles = callAllFunctionsAndMergeResults(
+        shadow,
+        borderMixIn,
+        background
+      )({ theme, ...pseudoStyles })
+
+      pseudo = `
+      ${pseudoSelectors[pseudoProp]}{ 
+        ${styles} 
+      }`
+    }
   }
 
   return clearEmptyLines(pseudo)
