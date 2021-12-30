fee956315284233128c34012f324b7b6c974b7f5
Jacek Kolasa <jacek.kolasa@gmail.com>
2021-12-30 11:08:06 +0100

    add icon-list story

---
+++ src/components/icon/icon-list.stories.js
@@ -0,0 +1,46 @@
+import React from "react"
+import styled from "styled-components"
+import { useCopyToClipboard } from "react-use"
+import { storiesOf } from "@storybook/react"
+
+import Flex from "src/components/templates/flex"
+import { Text, TextBigger } from "src/components/typography"
+
+import { getColor } from "../../theme"
+import { iconsList } from "./icons-list"
+import { Icon } from "."
+
+const iconListStory = storiesOf("COMPONENTS|Icons", module)
+
+const iconsExposed = Object.keys(iconsList)
+
+const Item = styled(Flex)`
+  &:hover {
+    background: ${getColor("mainBackgroundDisabled")};
+  }
+`
+
+iconListStory.add("Svgs", () => {
+  const [, copyToClipboard] = useCopyToClipboard()
+  return (
+    <Flex column padding={[2, 4]} gap={4}>
+      <TextBigger>Click item to copy icon name</TextBigger>
+      <Flex flexWrap>
+        {iconsExposed.map(name => (
+          <Item
+            onClick={() => copyToClipboard(name)}
+            cursor="pointer"
+            key={name}
+            padding={[3, 3]}
+            flex="grow"
+            basis="33%"
+            justifyContent="between"
+          >
+            <Text>{name}</Text>
+            <Icon name={name} />
+          </Item>
+        ))}
+      </Flex>
+    </Flex>
+  )
+})
