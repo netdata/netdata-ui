3cca8e42423c34bfcc995d6591938be9dd422e2f
Alexandros Ntitorss <a.ntitoras@gmail.com>
2022-01-12 10:57:23 +0200

    add space to filter box

--- src/components/filter-box/styled.js
+++ src/components/filter-box/styled.js
@@ -14,7 +14,7 @@ export const PlaceholderContainer = styled.div`
   height: 100%;
   position: absolute;
   top: 0;
-  left: 0;
+  left: 5px;
   right: 0;
   bottom: 0;
   padding-left: 6px;
@@ -38,7 +38,7 @@ export const FilterContainer = styled(Flex)`
     height: 100%;
     position: absolute;
     top: 0;
-    left: 0;
+    left: 5px;
     right: 0;
     bottom: 0;
     background: transparent;
