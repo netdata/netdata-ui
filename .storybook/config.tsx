3dd720ab39bf6df8c68636c6a5054bd879c921d1
AlexNti <47026269+AlexNti@users.noreply.github.com>
2022-01-17 14:08:06 +0200

    Feat/dynamically toggle theme (#259)
    * add button to change theme
    
    * replace button with toggle
    
    * save to localstorage the selection of the color

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,12 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import { addParameters, addDecorator, configure } from "@storybook/react"
 import { addReadme } from "storybook-readme"
 import { withTests } from "@storybook/addon-jest"
-import { withThemesProvider } from "storybook-addon-styled-component-theme"
 import { withKnobs } from "@storybook/addon-knobs"
 import centered from "@storybook/addon-centered/react"
 import { DefaultTheme } from "src/theme/default"
+import { DarkTheme } from "src/theme/dark"
+
 import { GlobalStyles } from "src/global-styles"
+import { ThemeProvider } from "styled-components"
+import Flex from "../src/components/templates/flex"
+import { Toggle } from "../src/components/toggle"
+import { useLocalStorage } from "react-use"
 
 const results = require("../.jest-test-results.json")
 
@@ -34,14 +39,36 @@ addParameters({
 
 addDecorator(addReadme)
 
-addDecorator(story => (
-  <>
-    <GlobalStyles />
-    {story()}
-  </>
-))
-
-addDecorator(withThemesProvider([DefaultTheme]))
+addDecorator(story => {
+  const [isDarkTheme, setIsDarkThem] = useLocalStorage("is_dark_theme")
+  const handleChange = e => {
+    setIsDarkThem(e.currentTarget.checked)
+  }
+  return (
+    <>
+      <ThemeProvider theme={isDarkTheme ? DarkTheme : DefaultTheme}>
+        <GlobalStyles />
+        <div id="story-wrapper" style={{ minHeight: "100vh" }}>
+          {story()}
+          <Flex>
+            <Toggle
+              labelRight={"Dark theme"}
+              labelLeft={"Light theme"}
+              onChange={handleChange}
+              checked={isDarkTheme}
+              colored={false}
+              disabled={false}
+              Label={"asdas"}
+              margin={""}
+              alignSelf={""}
+              className={""}
+            />
+          </Flex>
+        </div>
+      </ThemeProvider>
+    </>
+  )
+})
 
 const loadStories = () => {
   const req = require.context("../src", true, /\.stories\.(tsx|js)$/)
