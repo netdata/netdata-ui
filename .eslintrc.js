01834cd94d221ae764e148ed1355467ce0ce519c
burbuli8ra <marilena.ioannou.sg@gmail.com>
2022-01-24 18:28:23 +0200

    chore: update eslint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,22 @@ module.exports = {
     node: true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+      rules: {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "error"
+      },
+    }
+  ],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
