52fbd59eafa8e7ac953c7df9791c4a9f29223100
Jacek Kolasa <jacek.kolasa@gmail.com>
2022-02-15 10:35:04 +0100

    support StickyHeader property

--- src/components/table/components/table-container/styled.js
+++ src/components/table/components/table-container/styled.js
@@ -4,6 +4,7 @@ export const StyledTable = styled.table`
   width: 100%;
   height: 100%;
   border-collapse: collapse;
+  position: ${({ hasStickyHeader }) => hasStickyHeader && "relative"};
 `
 
 export const BlockLayout = styled.div`
