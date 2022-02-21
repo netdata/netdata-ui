69a6ec73e876af7b8388ab31d22d1bc2aa24d1b3
Jacek Kolasa <jacek.kolasa@gmail.com>
2022-02-21 10:07:07 +0100

    don't wrap text in pills (#284)
    * whiteSpace="nowrap" for all pills
    
    * prettify
    
    * 1.7.48

--- src/components/pill/index.js
+++ src/components/pill/index.js
@@ -12,20 +12,58 @@ const TextComponents = {
 }
 
 const Pill = forwardRef(
-  ({ children, background, color, flavour, hollow, icon, iconSize, normal, reverse, size, textSize, tiny, ...rest }, ref) => {
+  (
+    {
+      children,
+      background,
+      color,
+      flavour,
+      hollow,
+      icon,
+      iconSize,
+      normal,
+      reverse,
+      size,
+      textSize,
+      tiny,
+      ...rest
+    },
+    ref
+  ) => {
     const iconProps = { color, flavour, hollow, icon, size: iconSize }
 
-    if (tiny) return <Container tiny background={background} flavour={flavour} hollow={hollow} ref={ref} {...rest} />
+    if (tiny)
+      return (
+        <Container
+          tiny
+          background={background}
+          flavour={flavour}
+          hollow={hollow}
+          ref={ref}
+          {...rest}
+        />
+      )
 
-    const Text = textSize ? TextComponents[textSize] : (TextComponents[size] || TextComponents.default)
+    const Text = textSize
+      ? TextComponents[textSize]
+      : TextComponents[size] || TextComponents.default
 
     return (
-      <Container background={background} flavour={flavour} gap={1} hollow={hollow} ref={ref} size={size} {...rest}>
+      <Container
+        background={background}
+        flavour={flavour}
+        gap={1}
+        hollow={hollow}
+        ref={ref}
+        size={size}
+        {...rest}
+      >
         {!reverse && <PillIcon {...iconProps} />}
         {children && (
           <Text
             color={color || (hollow ? getPillColor("color", flavour) : "bright")}
             strong={!normal}
+            whiteSpace="nowrap"
           >
             {children}
           </Text>
