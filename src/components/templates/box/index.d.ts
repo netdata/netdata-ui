1a51654b6ea6eb48e32001cb4b1bd283b818b2d4
Alexandros Ntitorss <a.ntitoras@gmail.com>
2022-02-28 14:09:39 +0200

    revert Box index

--- src/components/templates/box/index.d.ts
+++ src/components/templates/box/index.d.ts
@@ -1,18 +1,6 @@
 import React from "react"
 
 import {
-  ColorType,
-  PaddingProps,
-  MarginProps,
-  RoundProps,
-  OpacityProps,
-  ZIndexProps,
-  PositionProps,
-  CursorProps,
-} from "src/mixins/types"
-import { CSSProperties } from "styled-components"
-
-import {
   AlignContentProps,
   AlignItemsProps,
   BorderProps,
@@ -26,13 +14,6 @@ import {
   WrapProps,
 } from "../mixins"
 
-type PostionDirection = {
-  top?: number | string
-  right?: number | string
-  left?: number | string
-  bottom?: number | string
-}
-
 export type NativeAttributes<El extends React.ElementType> = React.ComponentPropsWithRef<El>
 export type CustomProps = AlignContentProps &
   AlignItemsProps &
@@ -44,22 +25,13 @@ export type CustomProps = AlignContentProps &
   JustifyContentProps &
   OverflowProps &
   WidthProps &
-  WrapProps &
-  MarginProps &
-  PaddingProps &
-  CursorProps &
-  ColorType &
-  RoundProps &
-  OpacityProps &
-  ZIndexProps &
-  PositionProps &
-  PostionDirection
+  WrapProps
 
 export type CombinedStyledProps<El extends React.ElementType> = NativeAttributes<El> & CustomProps
 
 export type BoxProps<T extends React.ElementType = any> = CombinedStyledProps<T> & {
   as?: React.ElementType
-  sx?: CSSProperties
+  sx?: CombinedStyledProps<T>
 }
 
 declare const Box: React.FC<BoxProps<"div">>
