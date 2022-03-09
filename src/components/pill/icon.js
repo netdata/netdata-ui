4d6a40a3612e029ceb8ecc78fc346650548f61d1
burbuli8ra <marilena.ioannou.sg@gmail.com>
2022-03-09 13:46:58 +0200

    test: add PillIcon component

--- src/components/pill/icon.js
+++ src/components/pill/icon.js
@@ -2,16 +2,17 @@ import React from "react"
 import { Icon } from "src/components/icon"
 import { getPillColor } from "./mixins/colors"
 
-const PillIcon = ({ icon, color, hollow, flavour, size }) => {
+const PillIcon = ({ icon, color, 'data-testid': dataTestId, hollow, flavour, size }) => {
   if (!icon) return null
   if (typeof icon !== "string") return icon
 
   return (
     <Icon
-      width={size || "14px"}
-      height={size || "14px"}
-      name={icon}
       color={color || (hollow ? getPillColor("color", flavour) : "bright")}
+      data-testid={dataTestId || "pill-icon"}
+      height={size || "14px"}
+      width={size || "14px"}
+      name={icon}
     />
   )
 }
