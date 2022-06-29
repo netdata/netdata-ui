59bfcb064ad09cbf62dbf208ff16aeb614a2717b
Alexandros Ntitorss <a.ntitoras@gmail.com>
2022-06-29 12:03:52 +0300

    added confirm dialog

---
+++ src/components/confirmation-dialog/confirmation-dialog.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+import Modal, {
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+} from "src/components/modal"
+import Flex from "src/components/templates/flex"
+import { Button } from "src/components/button"
+
+const ConfirmationDialog = ({
+  title,
+  message,
+  confirmLabel,
+  declineLabel,
+  handleConfirm,
+  handleDecline,
+}) => {
+  return (
+    <Modal>
+      <ModalContent>
+        <ModalHeader>
+          {title}
+          <ModalCloseButton onClose={handleDecline} />
+        </ModalHeader>
+        <ModalBody>{message}</ModalBody>
+        <ModalFooter hasBorder={false}>
+          <Flex width="100%" justifyContent="end">
+            <Button label={confirmLabel} onClick={handleConfirm} />
+            <Button label={declineLabel} onClick={handleDecline} />
+          </Flex>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default ConfirmationDialog
