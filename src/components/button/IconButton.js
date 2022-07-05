9f0a43e5788adb2714fd7a90c151381d10e161a2
Alexandros Ntitorss <a.ntitoras@gmail.com>
2022-07-05 13:25:41 +0300

    add icon color

---
+++ src/components/button/IconButton.js
@@ -0,0 +1,29 @@
+import { Button } from "./button"
+import Box from "src/components/templates/box"
+
+import React from "react"
+
+const IconButton = ({
+  iconColor = "nodeBadgeColor",
+  flavour = "borderless",
+  icon,
+  disabled,
+  onClick,
+  width = "16px",
+  height = "18px",
+}) => {
+  return (
+    <Box
+      cursor="pointer"
+      sx={{ width, height }}
+      onClick={onClick}
+      as={Button}
+      flavour={flavour}
+      disabled={disabled}
+      icon={icon}
+      iconColor={iconColor}
+    ></Box>
+  )
+}
+
+export default IconButton
