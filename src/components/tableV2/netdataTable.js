f66e2fb72b9dc13b62f7893c6a0c8fe483afae3a
Alexandros Ntitorss <a.ntitoras@gmail.com>
2022-07-13 10:40:07 +0300

    remove functionallity at actions

--- src/components/tableV2/netdataTable.js
+++ src/components/tableV2/netdataTable.js
@@ -46,6 +46,7 @@ export const supportedBulkActions = {
   toggleAlarm: { icon: "alarm_off", confirmation: false, tooltipText: "Turn of Alarms" },
   userSettings: { icon: "user", confirmation: false, tooltipText: "User Settings" },
   addEntry: { icon: "plus", alwaysEnabled: true },
+  remove: { icon: "removeNode", confirmation: true },
 }
 
 export const supportedRowActions = {
@@ -63,6 +64,7 @@ export const supportedRowActions = {
   info: { icon: "information", confirmation: false, tooltipText: "Information" },
   toggleAlarm: { icon: "alarm_off", confirmation: false, tooltipText: "Turn of Alarms" },
   userSettings: { icon: "user", confirmation: false, tooltipText: "User Settings" },
+  remove: { icon: "removeNode", confirmation: true },
 }
 
 const table = createTable().setOptions({ filterFns: { comparison, select } })
