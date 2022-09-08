3f2ca7dc9a5693eb2d45cf3663bc600173bd8313
Alexandros Ntitorss <a.ntitoras@gmail.com>
2022-09-08 15:35:27 +0300

    check if there is background and apply it

--- src/components/pill/mixins/background.js
+++ src/components/pill/mixins/background.js
@@ -1,10 +1,14 @@
 import { getColor } from "src/theme"
 import { getMasterCardColor, getPillColor } from "./colors"
 
-export const getMasterCardBackground = (background, flavour) => background || getMasterCardColor(flavour)
+export const getMasterCardBackground = (background, flavour) =>
+  background || getMasterCardColor(flavour)
 
 const getPillBackground = ({ theme, background, flavour = "neutral", hollow }) => {
-  if (background) return
+  if (background) {
+    const value = getColor(background)({ theme })
+    return `background-color: ${value};`
+  }
   const type = hollow ? "hollow" : "background"
   const value = getColor(getPillColor(type, flavour))({ theme })
   return `background-color: ${value};`
