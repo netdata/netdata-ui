d18cdb711f5bfcd76a03ddd0b2c936deb9684213
burbuli8ra <marilena.ioannou.sg@gmail.com>
2022-10-13 14:30:09 +0300

    chore(drops): add hideShadow prop to menu dropdown component

--- src/components/drops/menu/dropdown.js
+++ src/components/drops/menu/dropdown.js
@@ -3,16 +3,18 @@ import styled from "styled-components"
 import Flex from "src/components/templates/flex"
 
 const Container = styled(Flex)`
-  box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2), 0 8px 10px 1px rgba(0, 0, 0, 0.14),
-    0 3px 14px 2px rgba(0, 0, 0, 0.12);
+  ${({ hideShadow }) =>
+    !hideShadow &&
+    "box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2), 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);"}
   list-style-type: none;
 `
 
-const Dropdown = ({ value, onItemClick, items, itemProps, renderItem }) => (
+const Dropdown = ({ hideShadow, itemProps, items, onItemClick, renderItem, value }) => (
   <Container
     as="ul"
     role="listbox"
     background="dropdown"
+    hideShadow={hideShadow}
     padding={[0]}
     margin={[1, 0]}
     column
