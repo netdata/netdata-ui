bfea44c284915f735c3df36c9be989ee997a62cd
Panos Christophides <panos@netdata.cloud>
2022-10-19 12:31:20 +0300

    update slider colors

--- src/components/input/range/styled.js
+++ src/components/input/range/styled.js
@@ -3,8 +3,8 @@ import styled from "styled-components"
 export const InputRange = styled.input.attrs({ type: "range" })`
   -webkit-appearance: none;
   height: 2px;
-  background: rgba(1, 135, 134, 0.26);
-  background-image: linear-gradient(#018786, #018786);
+  background: #00ab4440;
+  background-image: linear-gradient(#00ab44, #00ab44);
   background-repeat: no-repeat;
   cursor: pointer;
   width: 100%;
@@ -14,7 +14,7 @@ export const InputRange = styled.input.attrs({ type: "range" })`
     height: 10px;
     width: 10px;
     border-radius: 50%;
-    background: #018786;
+    background: #00ab44;
     transition: all 0.3s ease-in-out;
 
     &:active {
