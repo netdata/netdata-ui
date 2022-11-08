36baa491c2ec47b75dbf76e60d3090896a282418
burbuli8ra <marilena.ioannou.sg@gmail.com>
2022-11-08 06:31:16 +0200

    fix(ND table): update column visibilty dropdown to be scrollable

--- src/components/tableV2/components/columnsMenu.js
+++ src/components/tableV2/components/columnsMenu.js
@@ -4,59 +4,60 @@ import { Text, ListItem } from "src/components/typography"
 import { Checkbox } from "src/components/checkbox"
 import Flex from "src/components/templates/flex"
 
-import styled from "styled-components"
-
-const Dropdown = styled(Flex)`
-  box-shadow: 0px 10px 18px rgba(9, 30, 66, 0.15), 0px 0px 1px rgba(9, 30, 66, 0.31);
-`
-
 const ColumnsMenu = ({ parentRef, isOpen, columns, onClose }) => {
+  console.log({ isOpen, parentRefCurrent: parentRef.current })
   if (parentRef.current && isOpen)
     return (
-      <Drop target={parentRef.current} onClickOutside={onClose}>
-        <Dropdown background="dropdown" column round={1} width={50}>
-          <Flex column>
-            <Flex
-              padding={[2]}
-              border={{
-                size: "1px",
-                type: "solid",
-                side: "bottom",
-                color: "borderSecondary",
-              }}
-            >
-              <Text strong>Columns</Text>
-            </Flex>
-
-            {columns.map(column => {
-              {
-                return (
-                  column.getCanHide() && (
-                    <Flex
-                      padding={[2]}
-                      as={ListItem}
-                      alignItems="center"
-                      justifyContent="between"
-                      key={column.id}
-                      border={{
-                        size: "1px",
-                        type: "solid",
-                        side: "bottom",
-                        color: "borderSecondary",
-                      }}
-                    >
-                      <Checkbox
-                        checked={column.getIsVisible()}
-                        onChange={column.getToggleVisibilityHandler()}
-                        label={column.id}
-                      />
-                    </Flex>
-                  )
-                )
-              }
-            })}
+      <Drop
+        background="dropdown"
+        height={{ max: "100vh" }}
+        onClickOutside={onClose}
+        overflow={{ vertical: "auto" }}
+        round={1}
+        target={parentRef.current}
+        width={50}
+      >
+        <>
+          <Flex
+            padding={[2]}
+            border={{
+              size: "1px",
+              type: "solid",
+              side: "bottom",
+              color: "borderSecondary",
+            }}
+          >
+            <Text strong>Columns</Text>
           </Flex>
-        </Dropdown>
+
+          {columns.map(column => {
+            {
+              return (
+                column.getCanHide() && (
+                  <Flex
+                    padding={[2]}
+                    as={ListItem}
+                    alignItems="center"
+                    justifyContent="between"
+                    key={column.id}
+                    border={{
+                      size: "1px",
+                      type: "solid",
+                      side: "bottom",
+                      color: "borderSecondary",
+                    }}
+                  >
+                    <Checkbox
+                      checked={column.getIsVisible()}
+                      onChange={column.getToggleVisibilityHandler()}
+                      label={column.id}
+                    />
+                  </Flex>
+                )
+              )
+            }
+          })}
+        </>
       </Drop>
     )
 
