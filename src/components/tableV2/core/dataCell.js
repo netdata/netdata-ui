b6765dd6213fc56dbee74d56be54d1c3653a692b
Alexandros Ntitorss <a.ntitoras@gmail.com>
2022-11-18 18:40:47 +0200

    remove unsused imports

--- src/components/tableV2/core/dataCell.js
+++ src/components/tableV2/core/dataCell.js
@@ -1,12 +1,11 @@
 import React, { memo } from "react"
 
 import Table from "./base-table"
-import { SIZE_SUB_UNIT } from "src/theme/default/constants.js"
 
 const DataCell = ({ cell, testPrefix, flexRender }) => {
   return (
     <Table.Cell
-      width={cell.column.getSize() / SIZE_SUB_UNIT}
+      width={cell.column.getSize()}
       minWidth={cell.column.columnDef.minSize}
       maxWidth={cell.column.columnDef.maxSize}
       data-testid={`netdata-table-cell-${cell.column.columnDef.id}${testPrefix}`}
