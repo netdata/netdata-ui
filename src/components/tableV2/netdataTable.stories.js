17c97c9d9c2a16be107f6088e8ffb64b83d07ea0
Alexandros Ntitorss <a.ntitoras@gmail.com>
2022-11-28 12:00:08 +0200

    change netdata table stories width

--- src/components/tableV2/netdataTable.stories.js
+++ src/components/tableV2/netdataTable.stories.js
@@ -459,7 +459,7 @@ StoryTable.add("Full Table functionallity", () => {
   ]
 
   return (
-    <Box height="800px" width="800px">
+    <Box height="800px" width="1200px">
       <NetdataTable
         onClickRow={({ data, table, fullRow }) => {
           console.log(data, table, fullRow)
