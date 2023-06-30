5eea76485fe3ca9f89c9c125a267dc33230e0109
John Kapantzakis <kapantzak@gmail.com>
2023-06-30 14:58:59 +0300

    Add button group component (#476)
    * Add button group component
    
    * Export ButtonGroup component
    
    * v2.18.1

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,11 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import { storiesOf } from "@storybook/react"
 import styled from "styled-components"
 
 import { action } from "@storybook/addon-actions"
 import { text, boolean, select } from "@storybook/addon-knobs"
 import { readmeCleanup } from "utils/readme"
-import { Button, IconButton } from "."
+import { Button, IconButton, ButtonGroup } from "."
 import readme from "./README.md"
 import { iconsList } from "src/components/icon"
 
@@ -97,3 +97,24 @@ buttonStory.add(
   () => <IconButton cursor="pointer" icon="chevron_left" iconSize="small" tooltip="Previous" />,
   subData
 )
+
+buttonStory.add("Button Group", () => (
+  <ButtonGroup>
+    <Button label="One" />
+    <Button label="Two" />
+    <Button label="Three" />
+  </ButtonGroup>
+))
+
+const radioButtonItems = [
+  { label: "One", value: 1 },
+  { label: "Two", value: 2 },
+  { label: "Three", value: 3 },
+]
+
+buttonStory.add("Radio Button Group", () => {
+  const [checked, setChecked] = useState(1)
+  const onChange = value => setChecked(value)
+
+  return <ButtonGroup items={radioButtonItems} checked={checked} onChange={onChange} />
+})
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,2 +1,3 @@
 export { Button } from "./button"
 export { default as IconButton } from "./IconButton"
+export { ButtonGroup } from "./buttonGroup"
