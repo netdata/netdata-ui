10a160b72b5d17e16d8dac0d37c1c8794a080e2b
John Kapantzakis <kapantzak@gmail.com>
2023-06-30 10:55:24 +0300

    Export ButtonGroup component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { Button, IconButton } from "./components/button"
+export { Button, IconButton, ButtonGroup } from "./components/button"
 
 export { Sidebar, PortalSidebar } from "./components/sidebar"
 export { Icon, iconsList, IconComponents } from "./components/icon"
