61b5c30ceadb42377331ac6763fdc7525aa845f9
John Kapantzakis <kapantzak@gmail.com>
2026-04-16 22:48:00 +0300

    v5.4.5 (#618)
    * Add flavours to tab
    
    * v5.4.5
    
    * Update test snapshot

--- src/components/tabs/tab.js
+++ src/components/tabs/tab.js
@@ -3,8 +3,8 @@ import { StyledTab, StyledTabMenu } from "./styled"
 
 export const Tab = ({ index, isMenuItem, onChange, ...rest }) => {
   const onClick = useCallback(e => onChange && onChange(index || 0, e), [index, onChange])
-
   const TabComponent = isMenuItem ? StyledTabMenu : StyledTab
+
   return (
     <TabComponent
       justifyContent="center"
@@ -12,7 +12,7 @@ export const Tab = ({ index, isMenuItem, onChange, ...rest }) => {
       onClick={rest.disabled ? undefined : onClick}
       {...rest}
     >
-      {rest.label}
+      {typeof rest.label === "function" ? rest.label({ index, isMenuItem, ...rest }) : rest.label}
     </TabComponent>
   )
 }
